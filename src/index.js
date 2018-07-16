965c6ddc25d33744fec5acf3ef4ef9bc756ab4ab
fanie <faniepotgieter@gmail.com>
2018-07-16 11:59:08 +0200

    active the redux devtools extension

--- src/index.js
+++ src/index.js
@@ -14,7 +14,11 @@ const initialState = {
   ]
 };
 
-const store = createStore(reducers, initialState);
+const store = createStore(
+  reducers,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <Provider store={store}>
