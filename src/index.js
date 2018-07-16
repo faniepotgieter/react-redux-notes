f6eb2e37ac1ab57f6643434f3a028c3f5639edcd
fanie <faniepotgieter@gmail.com>
2018-07-16 11:05:52 +0200

    add a redux store with some default notes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import reducers from "./reducers";
 
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("app"));
+const initialState = {
+  notes: [
+    { title: "Title 1", body: "Body 1", timestamp: new Date() },
+    { title: "Title 2", body: "Body 2", timestamp: new Date() },
+    { title: "Title 3", body: "Body 3", timestamp: new Date() }
+  ]
+};
+
+const store = createStore(reducers, initialState);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("app")
+);
