ac0dbaf2b7c9ab68fac3692b9febdd2cc485a41b
fanie <faniepotgieter@gmail.com>
2018-07-17 13:27:52 +0200

    add some css resets

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
+import "./css/index.css";
 import reducers from "./reducers";
 
 import App from "./components/App";
