df591cd718709a88d20c5f5d9a2fffb08810ca10
fanie <faniepotgieter@gmail.com>
2018-07-19 11:08:39 +0200

    add tests to check if the App component contains the new components

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -3,11 +3,27 @@ import ReactDOM from "react-dom";
 import { mount } from "enzyme";
 import Root from "../../Root";
 import App from "../App";
+import Menu from "../Menu";
 import NotesList from "../NotesList";
+import NoteDetail from "../NoteDetail";
+
+let initialState = {};
+let wrapper;
 
 describe("App", () => {
+  beforeEach(() => {
+    wrapper = mount(
+      <Root initialState={initialState}>
+        <App />
+      </Root>
+    );
+  });
+
+  afterEach(() => {
+    wrapper.unmount();
+  });
+
   it("renders without crashing", () => {
-    const initialState = {};
     const div = document.createElement("div");
     ReactDOM.render(
       <Root initialState={initialState}>
@@ -17,14 +33,15 @@ describe("App", () => {
     );
   });
 
-  it("contains a notes list", () => {
-    const initialState = {};
+  it("contains a Menu component", () => {
+    expect(wrapper.find(Menu).length).toEqual(1);
+  });
 
-    const wrapper = mount(
-      <Root initialState={initialState}>
-        <App />
-      </Root>
-    );
+  it("contains a NotesList component", () => {
     expect(wrapper.find(NotesList).length).toEqual(1);
+  });
+
+  it("contains a NoteDetail component", () => {
+    expect(wrapper.find(NoteDetail).length).toEqual(1);
   });
 });
