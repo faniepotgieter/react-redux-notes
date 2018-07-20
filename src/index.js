eaaebc8242af1b7ecadba13ec54d1c2eb19abd65
fanie <faniepotgieter@gmail.com>
2018-07-20 12:32:34 +0200

    change the shape of the notes in the initial state

--- src/index.js
+++ src/index.js
@@ -6,12 +6,30 @@ import Root from "./Root";
 import App from "./components/App";
 
 const initialState = {
-  notes: [
-    { id: 1, title: "Title 1", body: "Body 1", edited: new Date() },
-    { id: 2, title: "Title 2", body: "Body 2", edited: new Date() },
-    { id: 3, title: "Title 3", body: "Body 3", edited: new Date() }
-  ],
-  selectedNote: 1
+  notes: {
+    byId: {
+      note1: {
+        id: "note1",
+        title: "Title 1",
+        body: "Body 1",
+        edited: new Date()
+      },
+      note2: {
+        id: "note2",
+        title: "Title 2",
+        body: "Body 2",
+        edited: new Date()
+      },
+      note3: {
+        id: "note3",
+        title: "Title 3",
+        body: "Body 3",
+        edited: new Date()
+      }
+    },
+    allIds: ["note1", "note2", "note3"]
+  },
+  selectedNote: "note1"
 };
 
 ReactDOM.render(
