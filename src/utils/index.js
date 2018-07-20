ef74ca11b1281129a5bbb3b7ba52394f37d5cfbd
fanie <faniepotgieter@gmail.com>
2018-07-20 17:08:30 +0200

    add two helper functions to format the dates & times for the edited field

---
+++ src/utils/index.js
@@ -0,0 +1,32 @@
+import dayjs from "dayjs";
+
+// use dayjs to get a short date that looks like one of these:
+// 5 PM  --> for today
+// Thu   --> for another day this week
+// 1 Jul --> for more than 7 days ago
+export const getShortDate = jsdate => {
+  let date = dayjs(jsdate);
+  let now = dayjs();
+  let diff = now.diff(date, "days");
+  let formatString = "D MMM h A";
+
+  if (now.day() !== date.day()) {
+    formatString = "ddd";
+  } else if (diff > 7) {
+    formatString = "D MMM";
+  } else {
+    formatString = "h A";
+  }
+
+  return date.format(formatString);
+};
+
+// use dayjs to get a long date like this:
+// 20 July 2018 at 17:03 PM
+export const getLongDate = jsdate => {
+  let date = dayjs(jsdate);
+
+  let formatString = "D MMMM YYYY [at] H:mm A";
+
+  return date.format(formatString);
+};
